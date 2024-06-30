86e8a5af9517988dd485e5ae96e5c25a28cfdaba
santiagoelautentico <santiago21062002@gmail.com>
2024-06-29 23:55:30 -0300

    filtros agregados

--- utils/functions.js
+++ utils/functions.js
@@ -1,5 +1,9 @@
-export const formatearFiltrosDB = (valor) =>
-  valor ? new RegExp(valor, "gi") : undefined;
+export const formatearFiltrosDB = (valor) => {
+  if (typeof valor === 'number') {
+    return valor; // Devuelve el número directamente
+  }
+  return valor ? new RegExp(valor, "gi") : undefined; // Devuelve la expresión regular para cadenas
+};
 
 export const obtenerProximoId = async (modelo) => {
   const ultimoId = await modelo.findOne().sort("-id").exec();
